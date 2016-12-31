9285a79f8a640145125267646b02762c7e1a9b07
Jaakko Komulainen <komulainenjaakko@gmail.com>
2016-12-31 09:12:02 +0000

    added gitignore and odoo manifest template.

---
+++ templates/manifest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': '',
+    'author': '',
+    'website': '',
+    # Categories can be used to filter modules in module listing
+    'category': '',
+    'version': '0.1',
+    # Any module necessary for this one to work correctly
+    'depends': [],
+    'data': [
+        # security/ir.model.access.csv,
+        # views/
+        # data/
+    ],
+    'description': '',
+}
